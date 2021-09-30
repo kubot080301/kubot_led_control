97468170efc9b597f6170c35b62a955bc39d029c
kubot080301 <kubot100601@gmail.com>
2021-09-30 18:32:57 +0800

    update ino

--- kubot_ws2812/arduino/Arduino_Nano/Arduino_Nano.ino
+++ kubot_ws2812/arduino/Arduino_Nano/Arduino_Nano.ino
@@ -75,11 +75,10 @@ void loop() {
       strip1.show();
       strip2.show();
       strip3.show();
-      strip4.show();}
-      inputString = "";
-      stringComplete = false;
-    
-
+      strip4.show();
+    }
+    inputString = "";
+    stringComplete = false;
   }
 }
 
